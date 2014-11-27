e39cc5cb28c1a1d0ce5e0157b568717c5ddca5ad
Shane Rogers <srogers@quickleft.com>
2014-11-26 17:10:12 -0800

    Trying to get web hook creation working

--- public/js/execute.js
+++
@@ -1,3 +0,0 @@
-$(function() {
-   console.log('executed just cause');
-});
--- public/js/webhooks.js
+++ public/js/webhooks.js
@@ -1,33 +1,48 @@
 /* add-on script */
 $(function() {
-
  AP.define('WebhookForm', {
-    buildWebhookForm: function() {
-      var form = ["<form class='aui'>",
-                "<div class='field-group'>",
-                    "<label for='comment-email'>Email <span class='aui-icon icon-required'>(required)</span></label>",
-                    "<input class='text medium-field' type='text' id='comment-email' name='comment-email' placeholder='you@example.com'>",
-                    "<div class='description'>Your primary email address.</div>",
-                "</div>",
-                "<div class='field-group'>",
-                    "<label for='comment-input'>Comment</label>",
-                    "<textarea class='textarea' name='comment-input' id='comment-input' placeholder='Your comment here...'></textarea>",
-                "</div>",
-                "<div class='buttons-container'>",
-                    "<div class='buttons'>",
-                        "<input class='button submit' type='submit' value='Save' id='comment-save-button'>",
-                        "<a class='cancel' href='#'>Cancel</a>",
-                    "</div>",
-                "</div>",
-            "</form>"]
-      from = form.join("\n")
-      $('.webhooks').html(form);
-      console.log('build the webhook form');
+
+    submitForm: function(event) {
+      // Extract the JWT token and other params
+      var qs = document.location.search;
+      qs = qs.split("+").join(" ");
+
+      var params = {}, tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+      while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] =
+          decodeURIComponent(tokens[2]);
+      }
+
+      AP.require('request', function(request) {
+        request({
+          url: '/rest/webhooks/1.0/webhook',
+          type: 'POST',
+          data: {
+            "name": "webhook",
+            "url": "http://requestb.in/1eivz7v1",
+            "events": ["jira:issue_created"],
+            "jqlFilter": "Project = JRA AND resolution = Fixed",
+            "excludeIssueDetails": false
+          },
+          contentType: "application/json",
+          success: function(responseText){
+            alert( "Success" + responseText );
+          },
+          error: function() {
+            alert( "Error" + arguments );
+          }
+        });
+      });
     }
+
  });
 
- AP.require('WebhookForm', function(WebhookForm) {
-    WebhookForm.buildWebhookForm();
- });
-
+  AP.require('WebhookForm', function(WebhookForm) {
+    $('#create-webhook').click(function( event ) {
+      event.preventDefault();
+      WebhookForm.submitForm();
+    });
+  });
 });
